b07d1d4354c949b2faa730f5db5341fdbdc5dd57
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-09-22 19:38:10 +0600

    user_table having a foreight key in database.

--- migrations/20250922123737_1_create_user_table.sql
+++ migrations/20250922123737_1_create_user_table.sql
@@ -1,5 +1,23 @@
 -- +goose Up
 -- +goose StatementBegin
+CREATE TABLE user_address (
+    id             SERIAL       PRIMARY KEY,
+    street_address VARCHAR(256) NOT NULL,
+    city           VARCHAR(128) NOT NULL,
+    state_         VARCHAR(128),
+    postal_code    VARCHAR(16),
+    country        VARCHAR(128) NOT NULL
+);
+-- +goose StatementEnd
+-- +goose StatementBegin
+CREATE TABLE emergency_contact (
+    id           SERIAL       PRIMARY KEY,
+    name_        VARCHAR(128) NOT NULL,
+    phone        VARCHAR(20)  NOT NULL,
+    relationship VARCHAR(64)
+);
+-- +goose StatementEnd
+-- +goose StatementBegin
 CREATE TABLE user_ (
     id            SERIAL       PRIMARY KEY,
     email         VARCHAR(128) UNIQUE NOT NULL,
@@ -8,43 +26,25 @@ CREATE TABLE user_ (
     first_name    VARCHAR(128) NOT NULL,
     last_name     VARCHAR(64)  NOT NULL,
     date_of_birth DATE         NOT NULL,
-    gender        CHAR(1)      NOT NULL,
+    gender        CHAR(1)      NOT NULL CHECK (gender IN ('M', 'F', 'O')),
     nationality   VARCHAR(16)  NOT NULL,
     created_on    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_login    TIMESTAMP
-);
--- +goose StatementEnd
--- +goose StatementBegin
-CREATE TABLE user_address (
-    id             SERIAL       PRIMARY KEY,
-    street_address VARCHAR(256) NOT NULL,
-    city           VARCHAR(128) NOT NULL,
-    state_         VARCHAR(128),
-    postal_code    VARCHAR(16),
-    country        VARCHAR(128) NOT NULL,
-    user_id        INTEGER      NOT NULL, 
-    FOREIGN KEY (user_id) REFERENCES user_(id) ON DELETE CASCADE
-);
--- +goose StatementEnd
--- +goose StatementBegin
-CREATE TABLE emergency_contact (
-    id           SERIAL       PRIMARY KEY,
-    name_        VARCHAR(128) NOT NULL,
-    phone        VARCHAR(20)  NOT NULL,
-    relationship VARCHAR(64),
-    user_id      INTEGER      NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user_(id) ON DELETE CASCADE
+    last_login    TIMESTAMP,
+    address_      INTEGER      NOT NULL,
+    emergency_    INTEGER      NOT NULL,
+    FOREIGN KEY (address_) REFERENCES user_address(id) ON DELETE SET NULL,
+    FOREIGN KEY (emergency_) REFERENCES emergency_contact(id) ON DELETE SET NULL
 );
 -- +goose StatementEnd
 
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS emergency_contact CASCADE;
+DROP TABLE IF EXISTS user_ CASCADE;
 -- +goose StatementEnd
 -- +goose StatementBegin
 DROP TABLE IF EXISTS user_address CASCADE;
 -- +goose StatementEnd
 -- +goose StatementBegin
-DROP TABLE IF EXISTS user_ CASCADE;
+DROP TABLE IF EXISTS emergency_contact CASCADE;
 -- +goose StatementEnd
