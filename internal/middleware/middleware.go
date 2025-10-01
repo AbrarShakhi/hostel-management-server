c579827d2541f056a0aa69aed68640ec04419e62
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-10-01 19:25:02 +0600

    imprv: add a function unauthorizedCookieError

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,8 +1,20 @@
 package middleware
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Middleware struct {
 }
 
 func NewMiddleware() *Middleware {
 	return &Middleware{}
+}
+
+func (m *Middleware) unauthorizedCookieError(c *gin.Context) {
+	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+		"msg": "cookie error",
+	})
 }
